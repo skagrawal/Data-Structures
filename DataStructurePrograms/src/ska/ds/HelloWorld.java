1fc87e307bd4650cd1c32d3f37c641eb5ed9924b
skagrawal <tech.shubham@gmail.com>
2016-01-05 23:11:37 -0600

    hello world test

---
+++ DataStructurePrograms/src/ska/ds/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ska.ds;
+
+/**
+ * @author Shubham
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
