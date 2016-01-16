bbf299b42c77f62be0cfd7992b40e125c94ecdd9
skagrawal <tech.shubham@gmail.com>
2016-01-15 23:18:16 -0600

    Add 2 numbers added

---
+++ DataStructurePrograms/src/ska/ds/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package ska.ds;
+
+import java.util.Random;
+
+/**
+ * @author Shubham (tech.shubham@gmail.com)
+ * Add two numbers without using any operator
+ */
+public class AddTwoNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.out.println(add(7,7));
+		Random rand = new Random();
+		for (int i = 0; i < 50; i++) {
+			add(rand.nextInt(100),rand.nextInt(1000));
+		}
+	}
+
+	private static int add(int x, int y) {
+
+		int carry = 0;
+		while (y != 0)
+	    {
+			//carry
+	        carry = x & y;  
+	        //sum
+	        x = x ^ y; 
+	        y = carry << 1;
+	    }
+		System.out.print(x+y +" ");
+		System.out.println(x);
+	    return x;
+	}
+
+
+}
