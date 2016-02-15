0062f314ef143b6231d3fc6688dc5a6436ad9c26
skagrawal <tech.shubham@gmail.com>
2016-02-14 18:40:36 -0600

    Added

--- DataStructurePrograms/src/ska/coms535/BloomFilter/BloomJoin.java
+++ DataStructurePrograms/src/ska/coms535/BloomFilter/BloomJoin.java
@@ -3,12 +3,22 @@
  */
 package ska.coms535.BloomFilter;
 
+import java.util.HashMap;
+
 /**
  * @author Shubham
  *
  */
 public class BloomJoin {
 	
-	
+	public static void main(String[] args) {
+		
+		
+		HashMap<String,Integer> map = new HashMap<String,Integer>();
+		map.put("de", 2);
+		map.put("3de", 2);
+		
+		System.out.println(map.size());
+	}
 
 }
---
+++ DataStructurePrograms/src/ska/ds/tree/LowestCommonAncestorBST.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package ska.ds.tree;
+
+/**
+ * @author Shubham (tech.shubham@gmail.com)
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+public class LowestCommonAncestorBST {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		BinarySearchTree bst = new BinarySearchTree();
+		for (int i = 0; i < 10; i++) {
+			bst.insert(i);
+		}
+		TreeNode root = bst.getRoot();
+		TreeNode res = lowestCommonAncestor(root,new TreeNode(5),new TreeNode(8));
+		System.out.println(res.val);
+	}
+	
+	
+	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	     
+        if(root == null){
+            return root;
+        }
+        //If both nodes value is greater than root then check in right subtree
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        //If both nodes value is smaller than root then check in left subtree
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        //If value of root is in between of two nodes value then return root
+        if(root.val > p.val && root.val < q.val){
+            return root;
+        }
+        return root;
+    }
+
+}
